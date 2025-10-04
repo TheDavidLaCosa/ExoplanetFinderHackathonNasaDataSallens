cab15798b990202923bb6efb23f1af1aded0cb63
David Larrosa <64459822+TheDavidLaCosa@users.noreply.github.com>
2025-10-04 13:08:53 +0200

    Backend multi image functionality

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,11 @@
+import base64
 from flask import Flask, jsonify, send_file
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_main():
+    return "Hello"
 
 # Data endpoint
 @app.route('/api/data', methods=['GET'])
@@ -21,6 +26,27 @@ def get_data():
 @app.route('/api/image', methods=['GET'])
 def get_image(name: str = ""):
     return send_file("nasa-october-2025-4k-3840x2160-1.webp", mimetype="image/webp")
+
+def image_to_base64(path):
+    with open(path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
+
+@app.route('/api/multi', methods=['GET'])
+def get_multiple_images():
+    # Llista d’imatges locals
+    images = ["example1.jpg", "example2.jpg", "example3.jpg"]
+
+    # Codifiquem cada imatge
+    encoded_images = [image_to_base64(img) for img in images]
+
+    # Retornem JSON amb text + imatges
+    response = {
+        "status": "success",
+        "message": "Tres imatges enviades correctament!",
+        "count": len(encoded_images),
+        "images": encoded_images
+    }
+    return jsonify(response)
 
 # Main
 if __name__ == '__main__':
